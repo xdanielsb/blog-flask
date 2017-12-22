0d8cb841a13ecea12daa791b3ba116304b6ea2c7
Daniel Santos <dfsantosb@correo.udistrital.edu.co>
2017-12-22 11:49:16 -0500

    Add meta data.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template
 
+#!/usr/bin/python
+__author__ = "Daniel Fernando Santos Bustos"
+__license__ = "MIT"
+__version__ = "1.0"
+__maintainer__ = "Daniel Santos"
+__email__ = "dfsantosbu@unal.edu.co"
+__status__ = "Development"
+
 app = Flask(__name__)
 app.config['DEBUG'] = False
 
