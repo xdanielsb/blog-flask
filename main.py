decb370939f3c223b01a4de9f23682483c11626d
Daniel Santos <dfsantosb@correo.udistrital.edu.co>
2017-12-22 12:55:09 -0500

    Add logic to Use the data from the API in the index.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
-
 #!/usr/bin/python
+
+from flask import Flask, render_template
+from constant import constants
+import random
+import urllib2
+import json
+
 __author__ = "Daniel Fernando Santos Bustos"
 __license__ = "MIT"
 __version__ = "1.0"
@@ -11,9 +16,23 @@ __status__ = "Development"
 app = Flask(__name__)
 app.config['DEBUG'] = False
 
+
+def getRandomColors( n = 30 ):
+    colors = []
+    for i in range(n):
+        r = lambda: random.randint(0,255)
+        color = '#%02X%02X%02X' % (r(),r(),r())
+        colors.append(color)
+    return colors
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    url  = constants["urlUsers"]
+    data = urllib2.urlopen(url)
+    result = json.load(data)
+    colors = getRandomColors()
+    return render_template('index.html', users=result, colors= colors)
 
 if __name__ =="__main__":
+    getRandomColors(10)
     app.run(debug=True)
