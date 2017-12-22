b3fc03c67688e8b2f227625c532049eb4fea5295
Daniel Santos <dfsantosb@correo.udistrital.edu.co>
2017-12-22 14:31:46 -0500

    Add logic in GUI to show the comments.

--- static/logic/requestComments.js
+++ static/logic/requestComments.js
@@ -1,35 +1,34 @@
 'use strict'
 
-let url_request_post =  "https://jsonplaceholder.typicode.com/posts?userId="
-function request_users_dto(response){
+
+function request_comment_dto(response){
   let payload = ""
   for( let id in response){
     console.log(response[id])
     let comment =  response[id]
     let content =   "<li class='collection-item avatar'>"+
                     "<i class='material-icons circle blue'>comment</i>"+
-                    "<span class='title'> <b>"+comment["title"]+" </b> <br> </span>"+
+                    "<span class='title'> <b>"+comment["email"]+" </b> <br> </span>"+
                     comment["body"]+
                     "</p>"+
                     "<a href='#!' class='secondary-content'><i class='material-icons'>visibility</i></a>"+
                     "</li>"
     payload += content
   }
-  document.getElementById("comments").innerHTML = payload
-
+  document.getElementById("infocomments").innerHTML = payload
 }
 
 //Create ajax JQuery request without token
-function request_post_dao( id ) {
+function request_comment_dao( id ) {
   let _method = "GET"
-  let _url = url_request_post + "" +id
+  let _url  =  "https://jsonplaceholder.typicode.com/posts/"+id+"/comments"
   $.ajax({
     method: _method,
     url: _url
   })
   .done((res) => {
     console.log("The request for :"+_url+ " has finished");
-    request_users_dto(res)
+    request_comment_dto(res)
 
   })
   .fail((_, status, error) => {
